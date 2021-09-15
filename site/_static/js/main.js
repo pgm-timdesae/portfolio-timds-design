d5003722e677f396407d869ebb7d956ae907c21a
pgm-timdesae <timdesae@student.ahs.be>
2021-09-15 15:29:34 +0200

    made the mobile navigation

---
+++ site/_static/js/main.js
@@ -0,0 +1,39 @@
+(() => {
+  const app = {
+    initialize () {
+      this.cacheElements();
+      this.registerEventListeners();
+    },
+    cacheElements () {
+      this.btnToTopElement = document.querySelector('.btn-to-top');
+      this.$nav = document.querySelector('.nav__list');
+      this.$burger = document.querySelector('.burger');
+      this.$navLinks = document.querySelectorAll('.nav__list-item');
+    },
+    registerEventListeners () {
+      // Burger icon
+      this.$burger.addEventListener('click', () => {
+
+        // toggle btn
+        this.$nav.classList.toggle('open');
+
+        // toggle icon
+        this.$burger.classList.toggle('toggle');
+      });
+
+
+      
+      // Back to top
+      /* if (this.btnToTopElement !== null) {
+        this.btnToTopElement.addEventListener('click', (ev) => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+        });
+      } */
+    },
+  };
+  app.initialize();
+})();
