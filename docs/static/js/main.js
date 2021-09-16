adc631dfbbe6bf6cb63775217fe8d420a4e57f34
pgm-timdesae <timdesae@student.ahs.be>
2021-09-16 12:54:20 +0200

    made footer

---
+++ docs/static/js/main.js
@@ -0,0 +1,42 @@
+(() => {
+  const app = {
+    initialize () {
+      this.cacheElements();
+      this.registerEventListeners();
+    },
+    cacheElements () {
+      this.btnToTopElement = document.querySelector('.btn-to-top');
+      this.$nav = document.querySelector('.nav__list');
+      this.$burger = document.querySelector('.burger');
+      this.$socialsNav = document.querySelector('.socials-nav');
+    },
+    registerEventListeners () {
+      // Burger icon
+      this.$burger.addEventListener('click', () => {
+
+        // toggle btn
+        this.$nav.classList.toggle('open');
+
+        //
+        this.$socialsNav.classList.toggle('open');
+
+        // toggle icon
+        this.$burger.classList.toggle('toggle');
+      });
+
+
+      
+      // Back to top
+      /* if (this.btnToTopElement !== null) {
+        this.btnToTopElement.addEventListener('click', (ev) => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+        });
+      } */
+    },
+  };
+  app.initialize();
+})();
